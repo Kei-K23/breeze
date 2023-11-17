6e42748f2ea62c2aaedd4c85488671e1efd1a86d
Kei-K23 <keiksl2301@gmail.com>
2023-11-17 15:02:50 +0630

    setup jwt

---
+++ server/src/lib/jwt.utils.ts
@@ -0,0 +1,46 @@
+import { sign, verify, SignOptions } from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export function signJWT({
+  payload,
+  secret,
+  options,
+}: {
+  payload: object;
+  secret: "ACCESS_TOKEN_SECRET" | "REFRESH_TOKEN_SECRET";
+  options?: SignOptions | undefined;
+}) {
+  const key =
+    secret === "ACCESS_TOKEN_SECRET"
+      ? process.env.ACCESS_TOKEN_SECRET
+      : process.env.REFRESH_TOKEN_SECRET;
+
+  return sign(payload, key as string, {
+    ...(options && options),
+    algorithm: "HS256",
+  });
+}
+
+export function verifyJWT<T>({
+  token,
+  secret,
+}: {
+  token: string;
+  secret: string;
+}) {
+  const key =
+    secret === "ACCESS_TOKEN_SECRET"
+      ? process.env.ACCESS_TOKEN_SECRET
+      : process.env.REFRESH_TOKEN_SECRET;
+
+  try {
+    const decoded = verify(token, key as string);
+    if (!decoded) {
+      return null;
+    }
+    return decoded as T;
+  } catch (e: any) {
+    return null;
+  }
+}
