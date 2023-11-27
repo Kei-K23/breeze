40a540c1c338c8fcf59363337025e54b0a85a275
Kei-K23 <keiksl2301@gmail.com>
2023-11-27 20:11:56 +0630

    fix 2

--- client/next.config.js
+++ client/next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "http://localhost:8090/api/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
